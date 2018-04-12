9fb21799fb490b5e188f00a93962357b76ccce04
Mikkel Olsen <mol@obviux.dk>
2018-04-12 15:30:47 +0200

    Changes to selenium

--- selenium/init.sh
+++ selenium/init.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 sudo chown seluser /etc/hosts
 getent hosts nginx | awk '{ print $1 "\tapp.dev.obviux.dk" }' >> /etc/hosts
 sudo chown root /etc/hosts
 
-/bin/sh /opt/bin/entry_point.sh
+/bin/bash /opt/bin/entry_point.sh
